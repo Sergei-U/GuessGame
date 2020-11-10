8097042d48e79885d89d50be8f814a77d67dfcfa
swizz <swizze@bk.ru>
2020-11-10 13:12:50 +0300

    Initial commit

---
+++ src/main/java/GameLauncher.java
@@ -0,0 +1,10 @@
+/**
+ * @author Sergei Usov
+ * @version 1.0.0
+ */
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame guessGame = new GuessGame();
+        guessGame.startGame();
+    }
+}
