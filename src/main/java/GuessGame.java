d8d35925264c948b6263362db666d0c01e4a1080
usov <usov@reksoft.ru>
2021-12-09 19:12:59 +0300

    2

--- src/main/java/GuessGame.java
+++ src/main/java/GuessGame.java
@@ -3,14 +3,11 @@
  * @version 1.0.0
  */
 public class GuessGame {
-    Player p1;
-    Player p2;
-    Player p3;
 
-    public void startGame(){
-        p1 = new Player();
-        p2 = new Player();
-        p3 = new Player();
+    public void startGame() {
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
 
         int guessp1 = 0;
         int guessp2 = 0;
@@ -20,11 +17,11 @@ public class GuessGame {
         boolean p2isRight = false;
         boolean p3isRight = false;
 
-        int targetNumber = (int) (Math.random()*10);
+        int targetNumber = (int) (Math.random() * 10);
         System.out.println("Я загадываю число от 0 до 9 ...");
 
         while (true) {
-            System.out.println("Число которое нужно угадать, - "+targetNumber);
+            System.out.println("Число которое нужно угадать, - " + targetNumber);
 
             p1.guess();
             p2.guess();
@@ -32,13 +29,13 @@ public class GuessGame {
 
 
             guessp1 = p1.number;
-            System.out.println("Первый игрок думает, что это "+guessp1);
+            System.out.println("Первый игрок думает, что это " + guessp1);
 
             guessp2 = p3.number;
-            System.out.println("Второй игрок думает, что это "+guessp2);
+            System.out.println("Второй игрок думает, что это " + guessp2);
 
             guessp3 = p3.number;
-            System.out.println("Третий игрок думает, что это "+guessp3);
+            System.out.println("Третий игрок думает, что это " + guessp3);
 
             if (guessp1 == targetNumber) {
                 p1isRight = true;
@@ -47,18 +44,17 @@ public class GuessGame {
                 p2isRight = true;
             }
             if (guessp3 == targetNumber) {
-                p3isRight =true;
+                p3isRight = true;
             }
             if (p1isRight || p2isRight || p3isRight) {
 
                 System.out.println("У нас есть победитель?");
-                System.out.println("Первый игрок угадал?"+p1isRight);
-                System.out.println("Второй игрок угадал?"+p2isRight);
-                System.out.println("Третий игрок угадал?"+p3isRight);
+                System.out.println("Первый игрок угадал?" + p1isRight);
+                System.out.println("Второй игрок угадал?" + p2isRight);
+                System.out.println("Третий игрок угадал?" + p3isRight);
                 System.out.println("Конец игры");
                 break;
-            }
-            else  {
+            } else {
                 System.out.println("Игроки должны попробовать еще раз");
             }
         }
